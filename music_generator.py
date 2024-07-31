e80aa13f7dc85fbebc19d6e0012824b8ada45999
unknown <igormelo9001@gmail.com>
2024-07-31 15:24:23 -0300

    fast training

--- music_generator.py
+++ music_generator.py
@@ -5,7 +5,7 @@ import librosa
 import soundfile as sf
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.losses import BinaryCrossentropy
 
 # Funções de Pré-processamento e Modelo
 def load_and_preprocess_audio(file_paths):
@@ -27,53 +27,70 @@ def create_sequences(data, seq_length):
             X.append(song[start:end])
     return np.array(X)
 
-def build_vae(input_shape):
-    inputs = layers.Input(shape=input_shape)
-    x = layers.Conv1D(64, 3, activation='relu', padding='same')(inputs)
-    x = layers.MaxPooling1D(2, padding='same')(x)
-    x = layers.Conv1D(128, 3, activation='relu', padding='same')(x)
-    x = layers.MaxPooling1D(2, padding='same')(x)
-    
-    shape_before_flattening = tf.keras.backend.int_shape(x)
-    x = layers.Flatten()(x)
-    
-    z_mean = layers.Dense(2)(x)
-    z_log_var = layers.Dense(2)(x)
-    
-    z = layers.Lambda(sampling, output_shape=(2,))([z_mean, z_log_var])
-    
-    encoder = models.Model(inputs, [z_mean, z_log_var, z], name='encoder')
-    
-    latent_inputs = layers.Input(shape=(2,))
-    x = layers.Dense(np.prod(shape_before_flattening[1:]), activation='relu')(latent_inputs)
-    x = layers.Reshape(target_shape=shape_before_flattening[1:])(x)
-    x = layers.Conv1DTranspose(128, 3, activation='relu', padding='same')(x)
-    x = layers.UpSampling1D(2)(x)
-    x = layers.Conv1DTranspose(64, 3, activation='relu', padding='same')(x)
-    x = layers.UpSampling1D(2)(x)
-    outputs = layers.Conv1DTranspose(input_shape[1], 3, activation='sigmoid', padding='same')(x)
-    
-    decoder = models.Model(latent_inputs, outputs, name='decoder')
-    
-    outputs = decoder(encoder(inputs)[2])
-    vae = models.Model(inputs, outputs, name='vae')
-    
-    reconstruction_loss = MeanSquaredError()(inputs, outputs)
-    kl_loss = -0.5 * tf.reduce_mean(z_log_var - tf.square(z_mean) - tf.exp(z_log_var) + 1)
-    vae_loss = tf.reduce_mean(reconstruction_loss + kl_loss)
-    
-    def vae_loss_fn(y_true, y_pred):
-        return MeanSquaredError()(y_true, y_pred) + kl_loss
-    
-    vae.compile(optimizer='adam', loss=vae_loss_fn)
-    return vae, encoder, decoder
+def build_gan(input_shape):
+    # Gerador
+    def build_generator():
+        model = models.Sequential([
+            layers.Input(shape=(100,)),
+            layers.Dense(input_shape[0] // 2 * input_shape[1] * 16, activation='relu'),
+            layers.Reshape((input_shape[0] // 2, input_shape[1] * 16)),
+            layers.Conv1DTranspose(128, kernel_size=5, activation='relu', padding='same'),
+            layers.UpSampling1D(2),
+            layers.Conv1DTranspose(64, kernel_size=5, activation='relu', padding='same'),
+            layers.Conv1DTranspose(input_shape[1], kernel_size=5, activation='sigmoid', padding='same')
+        ])
+        return model
 
-def sampling(args):
-    z_mean, z_log_var = args
-    batch = tf.shape(z_mean)[0]
-    dim = tf.shape(z_mean)[1]
-    epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+    # Discriminador
+    def build_discriminator():
+        model = models.Sequential([
+            layers.Input(shape=input_shape),
+            layers.Conv1D(64, kernel_size=5, strides=2, padding='same'),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv1D(128, kernel_size=5, strides=2, padding='same'),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Flatten(),
+            layers.Dense(1, activation='sigmoid')
+        ])
+        return model
+
+    generator = build_generator()
+    discriminator = build_discriminator()
+
+    # Compilação do Discriminador
+    discriminator.compile(optimizer='adam', loss=BinaryCrossentropy(), metrics=['accuracy'])
+
+    # GAN
+    z = layers.Input(shape=(100,))
+    generated_sequence = generator(z)
+    discriminator.trainable = False
+    validity = discriminator(generated_sequence)
+
+    gan = models.Model(z, validity)
+    gan.compile(optimizer='adam', loss=BinaryCrossentropy())
+    
+    return generator, discriminator, gan
+
+def train_gan(generator, discriminator, gan, X, epochs=1, batch_size=32):
+    for epoch in range(epochs):
+        # Treinar Discriminador
+        idx = np.random.randint(0, X.shape[0], batch_size)
+        real_sequences = X[idx]
+        fake_sequences = generator.predict(np.random.randn(batch_size, 100))
+
+        d_loss_real = discriminator.train_on_batch(real_sequences, np.ones((batch_size, 1)))
+        d_loss_fake = discriminator.train_on_batch(fake_sequences, np.zeros((batch_size, 1)))
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        
+        # Treinar Gerador
+        g_loss = gan.train_on_batch(np.random.randn(batch_size, 100), np.ones((batch_size, 1)))
+        
+        print(f"{epoch}/{epochs} [D loss: {d_loss[0]} | D accuracy: {100*d_loss[1]}] [G loss: {g_loss}]")
+
+def generate_music(generator, duration, seq_length):
+    noise = np.random.randn(duration * seq_length, 100)
+    generated_sequences = generator.predict(noise)
+    return generated_sequences
 
 class MusicGenApp:
     def __init__(self, root):
@@ -102,9 +119,9 @@ class MusicGenApp:
         self.duration_entry.pack(pady=5)
         
         self.selected_files = []
-        self.model = None
-        self.encoder = None
-        self.decoder = None
+        self.generator = None
+        self.discriminator = None
+        self.gan = None
         
     def select_files(self):
         files = filedialog.askopenfilenames(title="Selecione Músicas", filetypes=[("Arquivos de Áudio", "*.mp3 *.wav")])
@@ -123,13 +140,13 @@ class MusicGenApp:
         
         self.label.config(text="Construindo e treinando modelo...")
         input_shape = (X.shape[1], X.shape[2])
-        self.model, self.encoder, self.decoder = build_vae(input_shape)
-        self.model.fit(X, X, epochs=1, batch_size=32)
+        self.generator, self.discriminator, self.gan = build_gan(input_shape)
+        train_gan(self.generator, self.discriminator, self.gan, X, epochs=1, batch_size=32)
         
         self.label.config(text="Modelo treinado com sucesso!")
     
     def generate_music(self):
-        if self.model is None:
+        if self.generator is None:
             self.label.config(text="Modelo não treinado!")
             return
         
@@ -140,7 +157,7 @@ class MusicGenApp:
             return
         
         self.label.config(text="Gerando música...")
-        generated_sequences = generate_music_from_vae(self.decoder, 2, num_samples=duration * 5)
+        generated_sequences = generate_music(self.generator, duration, 30)
         generated_spectrogram = np.vstack(generated_sequences)
         
         self.label.config(text="Convertendo espectrograma para áudio...")
@@ -151,7 +168,7 @@ class MusicGenApp:
         self.label.config(text="Música gerada e salva com sucesso!")
 
     def generate_midi(self):
-        if self.model is None:
+        if self.generator is None:
             self.label.config(text="Modelo não treinado!")
             return
         
@@ -162,7 +179,7 @@ class MusicGenApp:
             return
         
         self.label.config(text="Gerando MIDI...")
-        generated_sequences = generate_music_from_vae(self.decoder, 2, num_samples=duration * 5)
+        generated_sequences = generate_music(self.generator, duration, 30)
         generated_spectrogram = np.vstack(generated_sequences)
         
         self.label.config(text="Convertendo espectrograma para MIDI...")
